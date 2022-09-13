59d62b49a89ef2a2a688081cd71d13c638937313
Goh Keng Boon Kelvin <d1349597@ncsdevnet.com>
2022-09-13 15:13:21 +0800

    updates from kelvin goh keng boon

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello World by Kelvin Goh Keng Boon!";
 
     public App() {}
 
